25a3f6d6135ffa308882cc6d0a6ed902fd0febc2
Lucio Henrique <luciolhl@outlook.com>
2024-06-11 16:54:03 -0300

    Alterado token da API

--- src/services/apiRanking.js
+++ src/services/apiRanking.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const apiRanking = axios.create({
-  baseURL: "http://server.pwvintage.com/api-ranking/index.php",
+  baseURL: "http://server.pwvintage.com/api/index.php",
   insecureHTTPParser: true,
   params: {
-    password: "P48AG4673eA41qWQeqe2YEDYJeteKKAtg",
+    password: "5mCjXZptT5fX4HdKwksjaiowlamso001823masd82kasd9al123",
   },
 });
 
